a024d6d3820bfc884da7d29dd7d3fc68aa38f9cf
Vicky Hu <whu8@lsu.edu>
2017-11-17 03:38:26 -0600

    11/17

---
+++ GUI/Assets/Script/SceneChange.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour {
+	public void Change (string SceneName){
+		Application.LoadLevel(SceneName);
+	}
+}
+
